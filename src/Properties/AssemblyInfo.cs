9a8ba173461ba20c409e8b8dfa4a7f1f4244a02d
Dmitriy Benyuk <dbe@dynamicweb-cms.com>
2023-01-25 10:24:00 +0200

    FixDWProvider

--- src/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Dynamicweb.DataIntegration.Providers.EcomProvider")]
-[assembly: InternalsVisibleTo("Dynamicweb.DataIntegration.Providers.OrderProvider")]
-[assembly: InternalsVisibleTo("Dynamicweb.Tests")]
